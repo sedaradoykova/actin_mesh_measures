72f3b20c70f5631bab16f196b2c7004beb6f9cb9
Seda Radoykova <seda.radoykova@gmail.com>
2023-02-05 00:20:33 +0000

    refactor visualisation for steerable_gauss_2order and add some input validation for visualise

--- meshwork/actinimg.py
+++ meshwork/actinimg.py
@@ -1,11 +1,11 @@
-import os
+import os, cv2
 import numpy as np 
 import matplotlib.pyplot as plt
 from matplotlib_scalebar.scalebar import ScaleBar
 from scipy.ndimage import correlate, morphology
 from skimage import measure
 from dataclasses import dataclass
-from actin_meshwork_analysis.meshwork.utils import get_image_stack, get_meta, get_resolution, get_fig_dims
+from meshwork.utils import get_image_stack, get_meta, get_resolution, get_fig_dims
  
 """ Design considerations: 
     Currently, the original data is kept and modifications happen on a copy which is internally updated 
@@ -71,27 +71,31 @@ class ActinImg:
             Position of scale bar; upper left by default. 
         """
         if not isinstance(imtype, str) or not isinstance(ind, int):
-            raise TypeError('type must be a string and ind must be an integer')
+            raise TypeError('imtype must be a string and ind must be an integer.')
         if (scale_bar and self.resolution is None):
             raise ValueError('Resolution has not been specified.')
+        if not isinstance(save, bool):
+            raise TypeError('save must be a boolean.')
+        if not os.path.exists(dest_dir):
+            raise ValueError(f'Directory not recognised: {dest_dir}.')
         if imtype=='manipulated':
             if self.manipulated_stack is None: 
                 raise ValueError('Raw data has not been normalised.')
             if ind < 1 or ind > self.manipulated_depth:
-                raise ValueError(f'ind must be an integer in range (1, {self.manipulated_depth}')        
+                raise ValueError(f'ind must be an integer in range (1, {self.manipulated_depth})')        
             if self._projected or self.manipulated_depth == 1:
                 plt.imshow(self.manipulated_stack, cmap=colmap)
             else: 
                 plt.imshow(self.manipulated_stack[ind-1], cmap=colmap)        
         elif imtype=='original':
             if ind < 1 or ind > self.depth:
-                raise ValueError(f'ind must be an integer in range (1, {self.depth}')  
+                raise ValueError(f'ind must be an integer in range (1, {self.depth})')  
             if self.depth > 1:
                 plt.imshow(self.image_stack[ind-1], cmap=colmap)
             else: 
                 plt.imshow(self.image_stack, cmap=colmap)
         else: 
-            raise ValueError('Image type \'{imtype}\' not recognised; type must be one of  [\'original\', \'manipulated\']'.format(imtype=imtype))
+            raise ValueError('Image type \'{imtype}\' not recognised; imtype must be one of [\'original\', \'manipulated\']'.format(imtype=imtype))
         if (scale_bar and self.resolution is not None):
             scalebar = ScaleBar(self.resolution, 'nm', box_color='None', color='#F2F2F2', location=bar_locate) 
             plt.gca().add_artist(scalebar)
@@ -129,12 +133,16 @@ class ActinImg:
             Position of scale bar; upper left by default. 
         """
         if not isinstance(imtype, str):
-            raise TypeError('type must be a string.')
+            raise TypeError('imtype must be a string.')
         if substack and (len(substack) != 2 or not isinstance(substack, list)): 
             raise ValueError('substack has to be a list of length=2, specifying a range.')
+        if not isinstance(save, bool):
+            raise TypeError('save must be a boolean.')
+        if not os.path.exists(dest_dir):
+            raise ValueError(f'Directory not recognised: {dest_dir}.')
         if imtype=='original':
-            if substack is not None and (substack[0] < 1 or substack[1] > self.manipulated_depth):
-                raise ValueError(f'substack must be integers in range (1, {self.manipulated_depth}).')
+            if substack is not None and (substack[0] < 1 or substack[1] > self.depth):
+                raise ValueError(f'substack must be a list of integers in range (1, {self.depth}).')
             if substack: 
                 data = self.image_stack[substack[0]-1:substack[1]].copy() 
                 figrows, figcols = get_fig_dims(substack[1]-substack[0]+1)
@@ -160,7 +168,7 @@ class ActinImg:
                 self.visualise('manipulated')
             else:
                 if substack is not None and (substack[0] < 1 or substack[1] > self.manipulated_depth):
-                    raise ValueError(f'substack must be integers in range (1, {self.manipulated_depth}).')
+                    raise ValueError(f'substack must be a list of integers in range (1, {self.manipulated_depth}).')
                 if substack: 
                     data = self.manipulated_stack[substack[0]-1:substack[1]].copy() 
                     figrows, figcols = get_fig_dims(substack[1]-substack[0]+1)
@@ -181,7 +189,7 @@ class ActinImg:
                     ax.text(0.66, 0.05, 'n={count}'.format(count=substack[0]+n),color='#F2F2F2',transform=ax.transAxes)
 
         else:
-            raise ValueError('Image type \'{imtype}\' not recognised; type must be one of  [\'original\', \'manipulated\']'.format(imtype=imtype))
+            raise ValueError('Image type \'{imtype}\' not recognised; imtype must be one of [\'original\', \'manipulated\']'.format(imtype=imtype))
 
         plt.subplots_adjust(wspace=0.01, hspace=0.01)
 
@@ -223,7 +231,7 @@ class ActinImg:
         if substack and (len(substack) != 2 or not isinstance(substack, list)): 
             raise ValueError('substack has to be a list of length=2, specifying a range.')
         if substack is not None and (substack[0] < 1 or substack[1] > self.manipulated_depth):
-            raise ValueError(f'substack must be integers in range (1, {self.manipulated_depth}).')
+            raise ValueError(f'substack must be a list of integers in range (1, {self.manipulated_depth}).')
         if substack: 
             data = self.manipulated_stack[substack[0]-1:substack[1]].copy() 
         else:
@@ -256,7 +264,7 @@ class ActinImg:
         if substack and (len(substack) != 2 or not isinstance(substack, list)): 
             raise ValueError('substack has to be a list of length=2, specifying a range.')
         if substack is not None and (substack[0] < 1 or substack[1] > self.manipulated_depth):
-            raise ValueError(f'substack must be integers in range (1, {self.manipulated_depth}).')
+            raise ValueError(f'substack must be a list of integers in range (1, {self.manipulated_depth}).')
         if substack: 
             data = self.manipulated_stack[substack[0]-1:substack[1]].copy()
         else:
@@ -285,7 +293,7 @@ class ActinImg:
         return None
 
         
-    def steerable_gauss_2order(self, substack=None, sigma: float=2.0, theta: float=0., visualise: bool=True):
+    def steerable_gauss_2order(self, substack=None, sigma: float=2.0, theta: float=0., visualise: bool=True, tmp: bool=False):
         """ Steers an X-Y separable second order Gaussian filter in direction theta.
         Implemented according to W. T. Freeman and E. H. Adelson, "The Design and Use of Steerable Filters", IEEE PAMI, 1991.
         Based on matlab code from Jincheng Pang, Tufts University, 2013.
@@ -295,29 +303,33 @@ class ActinImg:
         substack : list
             A list of length=2, specifying the range [start_index, finish_index] of slices to perform the operation on.
             Note: indexing is from 1 to `n_frames` in image stack. 
-        sigma : float
+        sigma : float=2.0
             The standard deviation of the Gaussian.
-        theta : float
+        theta : float=0.
             The steerable filter orientation. 
+        tmp : bool=False
+            Optional argument enables returning the response and the oriented filter without updating original object. 
 
         Returns
         -------
-        response : numpy array 
-            The response of the theta-rotated derivative. 
+        dictionary (optional)
+            Dictionary maps the response of the theta-rotated derivative and the oriented filter. 
         """
         if self.manipulated_stack is None: 
             raise ValueError('Raw data has not been normalised.')
         if substack and (len(substack) != 2 or not isinstance(substack, list)): 
             raise ValueError('substack has to be a list of length=2, specifying a range.')
         if substack is not None and (substack[0] < 1 or substack[1] > self.manipulated_depth):
-            raise ValueError(f'substack must be integers in range (1, {self.manipulated_depth}).')
+            raise ValueError(f'substack must be a list of integers in range (1, {self.manipulated_depth}).')
         if substack: 
             data = self.manipulated_stack[substack[0]-1:substack[1]].copy()
+            depth, rows, cols = substack[1]-substack[0]+1, *self.shape
         else:
             data = self.manipulated_stack.copy()
+            depth, rows, cols = self.manipulated_depth, *self.shape
 
         #### Separable filter kernels
-        # Gaussian filter mesh grid 
+        # Gaussian kernel mesh grid  
         Wx = np.floor((8/2)*sigma)
         Wx = Wx if Wx >= 1 else 1
 
@@ -326,75 +338,67 @@ class ActinImg:
 
         theta = np.deg2rad(-theta) # convert to radians, clockwise
 
-        # Filter kernels 
+
+        # Second derivative of the Gaussian
         g0 = np.array(np.exp(-(xx**2+yy**2)/(2*sigma**2))/(sigma*np.sqrt(2*np.pi)))
+        # Separable filter kernels 
         G2a = np.array(-g0/sigma**2+g0*xx**2/sigma**4)
         G2b =  np.array(g0*xx*yy/sigma**4)
         G2c = np.array(-g0/sigma**2+g0*yy**2/sigma**4)
 
         oriented_filter = np.array((np.cos(theta))**2*G2a + np.sin(theta)**2*G2c - 2*np.cos(theta)*np.sin(theta)*G2b)
-        
-        if self.manipulated_depth > 1: 
-            depth, rows, cols = data.shape
-        else:
-            depth, rows, cols = 1, *self.shape
+
         response_stack = np.empty((depth, rows, cols))
         response_stack.fill(np.nan)
+
         if depth > 1:
             for count, input in enumerate(data): 
                 #### Oriented filter response.
-                # Calculate image gradients, using separability.
-                I2a = correlate(input, G2a, mode='nearest')
-                I2b = correlate(input, G2b, mode='nearest')
-                I2c = correlate(input, G2c, mode='nearest')
+                # Calculate image gradients, using separability
+                # I2a = correlate(input, G2a, mode='nearest')
+                # I2b = correlate(input, G2b, mode='nearest')
+                # I2c = correlate(input, G2c, mode='nearest')
+                I2a = cv2.filter2D(input, -1, G2a, borderType=cv2.BORDER_REPLICATE)
+                I2b = cv2.filter2D(input, -1, G2b, borderType=cv2.BORDER_REPLICATE)
+                I2c = cv2.filter2D(input, -1, G2c, borderType=cv2.BORDER_REPLICATE)
 
                 # Evaluate oriented filter response.
                 response = np.array(
-                    (np.cos(theta))**2*I2a + np.sin(theta)**2*I2c - 2*np.cos(theta)*np.sin(theta)*I2b)
+                    (np.cos(theta))**2*I2a + np.sin(theta)**2*I2c - 2*np.cos(theta)*np.sin(theta)*I2b) #, dtype='uint16')
                 response_stack[count,:,:] = np.copy(response)
         else:
-            input = data.copy()
-            I2a = correlate(input, G2a, mode='nearest')
-            I2b = correlate(input, G2b, mode='nearest')
-            I2c = correlate(input, G2c, mode='nearest')
+            input = data[0,:,:]
+            # I2a = correlate(input, G2a, mode='nearest')
+            # I2b = correlate(input, G2b, mode='nearest')
+            # I2c = correlate(input, G2c, mode='nearest')
+            I2a = cv2.filter2D(input, -1, G2a, borderType=cv2.BORDER_REPLICATE)
+            I2b = cv2.filter2D(input, -1, G2b, borderType=cv2.BORDER_REPLICATE)
+            I2c = cv2.filter2D(input, -1, G2c, borderType=cv2.BORDER_REPLICATE)
 
             # Evaluate oriented filter response.
-            response = np.array(
-                (np.cos(theta))**2*I2a + np.sin(theta)**2*I2c - 2*np.cos(theta)*np.sin(theta)*I2b)
-            response_stack = np.copy(response)
+            response_stack[0,:,:] = np.array(
+                (np.cos(theta))**2*I2a + np.sin(theta)**2*I2c - 2*np.cos(theta)*np.sin(theta)*I2b) #, dtype='uint16')
+
 
         if visualise: 
-            figrows, figcols, left_to_del = get_fig_dims(depth*2+1) 
-            ratio = figrows/figcols
-            fig, axes = plt.subplots(nrows=figrows, ncols=figcols, figsize=(10*ratio,10*ratio))
-
-            if depth > 1:
-                for ax, image, title in zip(
-                    axes.flatten(), 
-                    [*np.rollaxis(data, 0), *np.rollaxis(response_stack, 0), oriented_filter], 
-                    [*['Input']*depth, *['Response']*depth, 'Oriented filter']
-                    ):
-                    ax.imshow(image, cmap='gray')
-                    ax.set_axis_off()
-                    ax.set_title(title)
-            else: 
-                for ax, image, title in zip(
-                    axes.flatten(), [data, response_stack, oriented_filter], 
-                    [*['Input']*depth, *['Response']*depth, 'Oriented filter']
-                    ):
-                    ax.imshow(image, cmap='gray')
-                    ax.set_axis_off()
-                    ax.set_title(title)
-            if left_to_del:
-                for ind1, ind2 in left_to_del:
-                    fig.delaxes(axes[ind1][ind2])
-
+            for n, (image, title) in enumerate(zip( 
+                [*np.rollaxis(data, 0), *np.rollaxis(response_stack, 0)],# oriented_filter], 
+                [*['Input']*depth, *['Response']*depth]# 'Oriented filter']
+                )):
+                ax = plt.subplot(2,depth,n+1)
+                ax.imshow(image, cmap='gray')
+                ax.set_axis_off()
+                ax.set_title(title)
+            plt.subplots_adjust(wspace=0.01, hspace=0.01)
             plt.show();
 
-        self.manipulated_stack = response_stack
-        self.manipulated_depth = depth
-        self._call_hist('steerable_gauss_2order')
-        return response_stack
+        if tmp: 
+            return {'response': response_stack, 'filter': oriented_filter}
+        else:
+            self.manipulated_stack = response_stack
+            self.manipulated_depth = depth
+            self._call_hist('steerable_gauss_2order')
+            return None
 
     
     def meshwork_density(self):
@@ -422,6 +426,7 @@ class ActinImg:
         """ Restores an ActImg instance to it's pre-manipulated state.
         """
         self.manipulated_stack = None
+        self.manipulated_depth = 0
         self._projected = None
         self._history = None
         return None
