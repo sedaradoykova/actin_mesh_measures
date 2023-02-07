4e65a798e3aab79ffdaa26d2df625275289715e9
Seda Radoykova <seda.radoykova@gmail.com>
2023-02-07 20:20:16 +0000

    confirm that mean of cv2 and scipy outputs match for normalised input data; see ./compare_cv2_scipy/

--- process_data/prelim_analysis.py
+++ process_data/prelim_analysis.py
@@ -17,55 +17,35 @@ if not os.path.exists(save_destdir):
     os.mkdir(save_destdir)
 
 
-"""normalise --> steer 2o Gauss --> min z proj --> threshold"""
+"""normalise --> steer 2o Gauss --> min z proj --> threshold; also max z proj on raw data"""
 theta_x6 = np.arange(0,360,60)
 
 actimg = get_ActinImg(filename, os.path.join(data_path,celltype))
-
 actimg.visualise_stack(imtype='original',save=True,dest_dir=save_destdir)
+
 
 actimg.normalise()
 actimg.steerable_gauss_2order_thetas(thetas=theta_x6,sigma=2,substack=[1,3],visualise=False)
 #actimg._visualise_oriented_filters(thetas=theta_x6,sigma=2,save=True,dest_dir=save_destdir)
-
 actimg.visualise_stack(imtype='manipulated',save=True,dest_dir=save_destdir)
+
 
 actimg.z_project_min()
 actimg.visualise_stack(imtype='manipulated',save=True,dest_dir=save_destdir)
+
 
 actimg.threshold(0.002)
 actimg.visualise_stack(imtype='manipulated',save=True,dest_dir=save_destdir)
 
 
-
-
-actimg.normalise()
-actimg.visualise_stack(imtype='manipulated', substack=[11,14])
-res = actimg.steerable_gauss_2order([1,3],tmp=True)
-actimg.visualise_stack(imtype='manipulated', substack=[1,3])
-
-actimg = get_ActinImg(filename, os.path.join(data_path,celltype))
-actimg.normalise()
-actimg.z_project_max()
-actimg.visualise('manipulated')
-
 actimg.nuke()
-actimg.normalise()
-
-actimg.visualise_stack(imtype='original', substack=[1,3])
-actimg.visualise_stack(imtype='manipulated')
+actimg.z_project_max()
+actimg.visualise_stack(imtype='manipulated',save=True,dest_dir=save_destdir)
 
 
+# # the output of cv2 and scipy functions matches for averaged out thetas with normalised image input
+# np.save('cv2.npy', actimg.manipulated_stack)
+# cv2_norm_mat = np.load('actin_meshwork_analysis\\process_data\\sample_data\\compare_cv2_scipy\\cv2_normalised.npy')
+# scipy_norm_mat = np.load('actin_meshwork_analysis\\process_data\\sample_data\\compare_cv2_scipy\\scipy_normalised.npy')
 
-actimg.visualise('manipulated')
-actimg.threshold(0.03)
-actimg.visualise('manipulated')
-
-
-actimg = get_ActinImg(filename, os.path.join(data_path,celltype))
-actimg.normalise()
-#actimg._visualise_oriented_filters(np.arange(0,360,60),2)
-actimg.steerable_gauss_2order_thetas(np.arange(0,360,60),2,[1,3], visualise=False)
-actimg.z_project_min()
-actimg.threshold(0.003)
-actimg.visualise('manipulated')
+# np.allclose(cv2_norm_mat, scipy_norm_mat, atol=1e-15)
